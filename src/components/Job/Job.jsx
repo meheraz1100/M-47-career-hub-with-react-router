b439d1a6039344835e121a37328d4f7b4c167f50
meheraz1100 <meheraz1100@gmail.com>
2024-04-15 22:20:27 +0600

    mapping each jobs

---
+++ src/components/Job/Job.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { HiOutlineCurrencyDollar } from "react-icons/hi";
+
+const Job = ({ job }) => {
+  const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
+
+  return (
+    <div className="card card-compact bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src={logo}
+          alt="logo"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{job_title}</h2>
+        <p>{company_name}</p>
+        <div>
+            <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
+            <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
+        </div>
+        <div className="mt-4 flex">
+            <h2 className="flex mr-4"><MdOutlineLocationOn className="text-2xl mr-2"></MdOutlineLocationOn>{location}</h2>
+            <h2 className="flex"><HiOutlineCurrencyDollar className="text-2xl mr-2"></HiOutlineCurrencyDollar>{salary}</h2>
+        </div>
+        <div className="card-actions">
+          <button className="btn btn-primary">View Details</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Job;
