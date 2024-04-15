d32c17829fea85a7d5a47f7d58de1966ce12ef48
meheraz1100 <meheraz1100@gmail.com>
2024-04-15 22:20:07 +0600

    show the components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import CatagoryList from '../catagoryList/CatagoryList';
+import FeaturedJobs from '../FeatureedJobs/FeaturedJobs';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Banner></Banner>
+            <CatagoryList></CatagoryList>
+            <FeaturedJobs></FeaturedJobs>
         </div>
     );
 };
