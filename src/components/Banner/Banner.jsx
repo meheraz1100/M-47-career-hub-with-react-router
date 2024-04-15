b99e384c3d7df2f4be7c98e67c86aba552d1db54
meheraz1100 <meheraz1100@gmail.com>
2024-04-15 22:17:24 +0600

    banner component added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+
+            <h2 className='text-center text-5xl'>Home Page Banner</h2>
+        </div>
+    );
+};
+
+export default Banner;
